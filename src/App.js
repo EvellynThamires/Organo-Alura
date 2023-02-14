ad07f337a4f17e3d4f8d375432bab2eaf3fb09cf
Evellyn Thamires <xe.uyemura@gmail.com>
2023-02-14 20:45:00 -0300

    adding banner, textField and form components and styles

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Banner from './components/Banner/Banner';
+import Form from './components/Form/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+const Banner = () => {
+    return(
+        <header className="banner">
+            <img src="/images/banner.png" alt="organo's main banner"/>
+        </header>
+    )
+}
+
+export default Banner
